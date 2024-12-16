a120c8be32b587a4d9dc19e18685c57274f40190
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2024-12-16 09:25:27 -0300

    Uso de controladores con paquete http

--- main.go
+++ main.go
@@ -38,13 +38,25 @@ func handler (w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hola %s!", "mundo")
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/view/"):] // obtenemos el título de la página a cargar.
+	p, err := loadPage(title) // cargamos la página.
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound) // si hay un error, redireccionamos al editor de la página.
+		return
+	}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body) // si no hay error, mostramos el contenido de la página.
+}
+
 func main(){
 	/* p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body)) // si no lo casteo a string, me imprime el slice de bytes. */
 
-	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", handler)
+
+	http.HandleFunc("/view/", viewHandler)
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
 	
